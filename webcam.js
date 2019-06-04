9a9c21880e7193ee8af3a3561957e13aa07db308
Richard Young <richard.young2@ibm.com>
2019-06-04 15:17:02 +0200

    Code clean up

---
+++ webcam.js
@@ -0,0 +1,18 @@
+async function setupWebcam(webcamElement) {
+    return new Promise((resolve, reject) => {
+        const navigatorAny = navigator;
+        navigator.getUserMedia = navigator.getUserMedia ||
+            navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
+            navigatorAny.msGetUserMedia;
+        if (navigator.getUserMedia) {
+            navigator.getUserMedia({ video: true },
+                stream => {
+                    webcamElement.srcObject = stream;
+                    webcamElement.addEventListener('loadeddata', () => resolve(webcamElement), false);
+                },
+                error => reject());
+        } else {
+            reject();
+        }
+    });
+}
